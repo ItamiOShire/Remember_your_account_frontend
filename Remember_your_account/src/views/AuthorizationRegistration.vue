3f590ee7bd88bf180ad59c623cfcb22942c96a50
Adam <a.niewczas01@gmail.com>
2025-03-07 21:23:29 +0100

    add: authorization pages

---
+++ Remember_your_account/src/views/AuthorizationRegistration.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <main-layout>
+
+        <section class="img-section">
+
+            <div class="img-container">
+
+                <img src="../assets/remgemLogo.png" alt="logo" class="img">
+
+            </div>
+
+        </section>
+
+        <section class="form-section">
+
+            <auth-form>
+
+                <form-input 
+
+                    :inputLabel = "'First Name'"
+                    :inputId = "'firstName'"
+                    :inputType = "'text'"
+                    :isRequired = "true"
+                    :inputPlaceholder = "'Enter your first name'"
+                    v-model:inputValue = "user.firstName"
+
+                />
+
+            </auth-form>
+
+        </section>
+
+    </main-layout>
+
+</template>
+
+<script>
+
+
+
+</script>
