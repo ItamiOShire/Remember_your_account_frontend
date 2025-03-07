a3c74cd4b758fb3c2c8881f033f244c3f0ef1af0
Adam <a.niewczas01@gmail.com>
2025-03-07 21:23:12 +0100

    add: home page

---
+++ Remember_your_account/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <MainLayout>
+
+        <auth-card>
+
+            <p class="card-text-container">
+
+            Don't have an account? <br>
+            Register now!
+
+            </p>
+
+            <Button>Register</Button>
+
+        </auth-card>
+
+        <auth-card>
+
+            <p class="card-text-container">
+
+            Log in now and recall <br>
+            your game accounts!
+
+            </p>
+
+            <Button>Log in</Button>
+
+        </auth-card>
+
+    </MainLayout>
+
+</template>
