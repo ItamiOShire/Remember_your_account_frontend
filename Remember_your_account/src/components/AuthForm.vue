4dad0e7a20d37f0d7a91ece5fd7c3245dad417e7
Adam <a.niewczas01@gmail.com>
2025-03-07 21:22:35 +0100

    Add: from component and style variables for it

---
+++ Remember_your_account/src/components/AuthForm.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="auth-form-container">
+
+        <form class="auth-form" :name="formName">
+
+            <slot></slot>
+
+        </form>
+
+    </div>
+
+</template>
+
+<script setup>
+
+    defineProps({
+
+        formName: {
+            type: String,
+            required: true
+        }
+
+    });
+
+</script>
+
+<style>
+
+    div.auth-form-container {
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        background-color: var(--formBgColor);
+        border-radius: var(--formBorderRadius); 
+
+    }
+
+</style>
